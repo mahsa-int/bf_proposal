24a7a99213a108c7f238b3cdd873df3e8682ccf0
daniel.munevar <daniel.munevar@rappi.com>
2024-07-05 16:02:03 -0500

    anthropic added

--- classes/anthropic.py
+++ classes/anthropic.py
@@ -1,15 +1,14 @@
 from langchain_anthropic import ChatAnthropic
 from langchain.prompts import ChatPromptTemplate
 
-MAX_TOKENS = 300
+MAX_TOKENS = 350
 
 def analyze_clusters(cluster_data, client):
     chat = ChatAnthropic(model="claude-3-5-sonnet-20240620", temperature=0.1, anthropic_api_key=client.api_key,max_tokens=MAX_TOKENS)
 
     prompt = ChatPromptTemplate.from_template("""
-    Analiza los siguientes datos del modelo de clustering y proporciona recomendaciones de estrategia de marketing para los 3 clusters mas relevantes. 
-    Identifica la característica más distintiva. Prioriza insights accionables y específico y justificables, Limita tu respuesta a 300 tokens y 
-    Usa un parrafo por cluster.
+    Analiza los siguientes datos del modelo de clustering y proporciona recomendaciones de marketing para los 3 clusters mas relevantes. 
+    Prioriza insights accionables y específico y justificables, Limita tu respuesta a 150 tokens por cluster.
 
     Datos de clustering:
     {cluster_data}
